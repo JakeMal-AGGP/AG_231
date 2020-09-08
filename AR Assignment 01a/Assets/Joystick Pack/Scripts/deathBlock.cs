864952228d9b9a2318ac5835dc4bf1807eacba19
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2020-09-08 14:11:23 -0400

    unity upload

---
+++ AR Assignment 01a/Assets/Joystick Pack/Scripts/deathBlock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathBlock : MonoBehaviour
+{
+
+    public CameraScript cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.transform.name == "Player")
+        {
+
+            Destroy(other.gameObject);
+            cam.isMoving = false;
+            cam.lose();
+            Debug.Log("Game over");
+
+        }
+
+    }
+
+}
---
+++ AR Assignment 01a/Assets/Joystick Pack/Scripts/winBlock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winBlock : MonoBehaviour
+{
+
+    public CameraScript cam;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.transform.name == "Player")
+        {
+
+            Debug.Log("You win!");
+            Destroy(other.gameObject);
+            cam.isMoving = false;
+            cam.win();
+
+        }
+
+    }
+}
